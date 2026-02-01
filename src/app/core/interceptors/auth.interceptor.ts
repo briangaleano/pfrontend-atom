85e2c2c6acb3cf6b99efb44c0de0766681d921cb
briangaleano <brian_g225@hotmail.com>
2026-01-31 23:53:42 -0600

    update url

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,10 +1,19 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../../login/services/auth.service';
+import { environment } from '../../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
   const token = auth.getToken();
+
+  let apiReq = req;
+
+  if (req.url.startsWith('/')) {
+    apiReq = req.clone({
+      url: `${environment.apiUrl}${req.url}`
+    });
+  }
 
   if (!token) {
     return next(req);
